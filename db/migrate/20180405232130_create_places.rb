a9031d5a0d22b9d95771ca956c27e7fb0ecea7c1
Ryan Schmelter <rschmelter33@gmail.com>
2018-04-05 23:31:54 +0000

    DB. Add Places Table

--- db/migrate/20180405232130_create_places.rb
+++ db/migrate/20180405232130_create_places.rb
@@ -1,4 +1,8 @@
 class CreatePlaces < ActiveRecord::Migration[5.1]
   def change
+    create_table :places do |t|
+      t.string :type
+      t.string :description
+      t.integer :area_id
   end
 end
