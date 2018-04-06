c34a8ebad149aae41c63b7e2c95a889363a47660
Ryan Schmelter <rschmelter33@gmail.com>
2018-04-06 18:11:47 +0000

    Controllers/Users add post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,21 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'users/new'
+      erb :'/users/new'
     else
       redirect to '/areas'
     end
+
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to '/areas'
+    end
+  end
 
 end
