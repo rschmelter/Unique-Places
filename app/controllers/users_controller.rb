329af4c81abef3b072a5ed14edd5e664c19fcc8d
Ryan Schmelter <rschmelter33@gmail.com>
2018-04-07 16:27:04 +0000

    Set up login, logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,30 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/areas'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/areas"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
+  end
+
+
+
+
 end
