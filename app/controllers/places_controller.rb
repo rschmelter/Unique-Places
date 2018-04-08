d14f4ac262ce2a9c0487a408623a419179800d8a
Ryan Schmelter <rschmelter33@gmail.com>
2018-04-08 18:06:04 +0000

    Controller/places add get for index

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,12 @@
 class PlacesController < ApplicationController
 
+  get '/places' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      @places = Place.all
+      erb :'places/index'
+    end
+  end
 
 end
